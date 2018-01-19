2ccef1e44ed52716fc8adeae7b709757105b964a
sdeitech <sdtech@smartdatainc.net>
2018-01-19 11:41:25 +0530

    ssl added

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var __dirname = '/home/anubhavg/Desktop/socket-text-chat/';
-//var __dirname = './socket-text-chat'
+var crypto = require('crypto');
+var fs = require("fs");
+var http = require('http');
+var https = require('https');
+var env = app.get('env');
+var __dirname = '/home/anubhavg/Desktop/socket-simpleWebRTC-chat/';
+var privateKey = '/home/anubhavg/Desktop/socket-simpleWebRTC-chat/stagingsdei_com.key';
+var cert = '/home/anubhavg/Desktop/socket-simpleWebRTC-chat/c86aaff33f318ca4.crt';
+
+if(env == 'production'){
+  var __dirname = '/home/SocketChat/socket-simpleWebRTC-chat';  
+  var privateKey = 'home/SocketChat/socket-simpleWebRTC-chat/stagingsdei_com.key';
+  var cert = 'home/SocketChat/socket-simpleWebRTC-chat/c86aaff33f318ca4.crt';  
+}
+
+var options = {
+  key: privateKey,
+  cert: cert
+};
 
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+console.log('env',app.get('env'));
 app.get('/video_call', function(req, res){
   res.sendFile(__dirname + '/simpleRTC.html');
 });
@@ -19,6 +35,12 @@ console.log('connected');
       io.emit('chat message', msg);
     });
   });
-http.listen(4141, function(){
-  console.log('listening on *:4141');
-});
+
+// Create an HTTP service.
+//http.createServer(app).listen(3000);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(4141);
+
+// http.listen(4141, function(){
+//   console.log('listening on *:4141');
+// });
