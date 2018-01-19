eab67591acef656739dba40236b9312bbec82191
sdeitech <sdtech@smartdatainc.net>
2018-01-19 15:41:57 +0530

    socket changes

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ var env = app.get('env');
 // var privateKey = '/home/anubhavg/Desktop/socket-simpleWebRTC-chat/stagingsdei_com.key';
 // var certificate = '/home/anubhavg/Desktop/socket-simpleWebRTC-chat/c86aaff33f318ca4.crt';
 
-  var __dirname = '/home/SocketChat/socket-simpleWebRTC-chat/';  
-  var privateKey = '/home/SocketChat/socket-simpleWebRTC-chat/stagingsdei_com.key';
-  var certificate = '/home/SocketChat/socket-simpleWebRTC-chat/c86aaff33f318ca4.crt';  
+var __dirname = '/home/SocketChat/socket-simpleWebRTC-chat/';  
+var privateKey = '/home/SocketChat/socket-simpleWebRTC-chat/stagingsdei_com.key';
+var certificate = '/home/SocketChat/socket-simpleWebRTC-chat/c86aaff33f318ca4.crt';  
 
 
 var options = {
   key: fs.readFileSync(privateKey),
   cert: fs.readFileSync(certificate)
 };
+//Create an HTTP service.
+//var server = http.createServer(app);
+// Create an HTTPS service identical to the HTTP service.
+var server = https.createServer(options, app);
 
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -27,16 +31,11 @@ console.log('env',env);
 
 io.on('connection', function(socket){
 console.log('connected');
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+
+socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
-
-//Create an HTTP service.
-//http.createServer(app).listen(3000);
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(4141);
-
-// http.listen(4141, function(){
-//   console.log('listening on *:4141');
-// });
+});
+//server.listen(3000);
+server.listen(4141);
+  
